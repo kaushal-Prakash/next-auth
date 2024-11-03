2209fd7e7f38a19c24350f03a9472215eed7133e
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-03 18:56:04 +0530

    user login and signup with forgot pass

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect(){
+    try {
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log("MongoDB connected !");
+        });
+
+        connection.on('error', (err) => {
+            console.log("MongoDB connection error, make sure DB is up and running !");
+            console.log(err);
+            process.exit(1); // exit with error
+        })
+    } catch (error) {
+        console.log("Something went wrong in connecting DB");
+        console.log(error);
+    }
+}
