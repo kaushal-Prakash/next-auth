b4235a09d0982905c190637cd03b0dae6de34dae
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-04 18:51:08 +0530

    final commit

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-export async function connect(){
-    try {
-        mongoose.connect(process.env.MONGO_URL!);
-        const connection = mongoose.connection;
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log("MongoDB connected !");
-        });
+    connection.on("connected", () => {
+      console.log("MongoDB connected !");
+    });
 
-        connection.on('error', (err) => {
-            console.log("MongoDB connection error, make sure DB is up and running !");
-            console.log(err);
-            process.exit(1); // exit with error
-        })
-    } catch (error) {
-        console.log("Something went wrong in connecting DB");
-        console.log(error);
-    }
+    connection.on("error", (err) => {
+      console.log("MongoDB connection error, make sure DB is up and running !");
+      console.log(err);
+      process.exit(1); // exit with error
+    });
+  } catch (error) {
+    console.log("Something went wrong in connecting DB");
+    console.log(error);
+  }
 }
